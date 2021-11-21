94b750eeeace75fff62aba6d4a2ea44bb4b528f8
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2021-11-21 08:11:12 +0000

    User Story 2 As an air traffic controller So I can get passengers on the way to their destination I want to instruct a plane to take off from an airport

--- spec/features/airport_spec.rb
+++ spec/features/airport_spec.rb
@@ -1,10 +1,15 @@
 require './lib/airport'
 
 describe Airport do
-	# it { is_expected.to respond_to(:land).with(1).argument } first scafolding block
+	# it { is_expected.to respond_to(:land).with(1).argument } first scafolding code
 
-	subject(:airport) { described_class.new }
-	it 'instruct a plane to land' do
-		expect(airport).to respond_to(:land).with(1).argument
-	end
+	 subject(:airport) { described_class.new }
+
+	 it 'instruct a plane to land' do
+ 		 expect(airport).to respond_to(:land).with(1).argument
+ 	end
+
+	 it 'instruct a plane to take off' do
+ 		 expect(airport).to respond_to(:take_off).with(1).argument
+ 	end
 end
